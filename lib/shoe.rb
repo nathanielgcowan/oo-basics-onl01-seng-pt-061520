86938dbc1f8adf3782c8b261d8dbd1c103e0766a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-07-05 19:20:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,40 +1,4 @@
 # Make your shoe class here!
 
 class Shoe
-  
-  def initialize(title) 
-    @title=title
-  end
-  
-  def title
-    @title
-  end
-  
-  def color=(author)
-    @author = color
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @page_count = num
-  end
- 
-  def page_count
-    @page_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
 end
